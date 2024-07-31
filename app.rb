8701100c322e1d86121afcde9546e1a8eb06c0a2
dn.artego <dn.artego@gmail.com>
2024-07-31 23:06:36 +0300

    validate

--- app.rb
+++ app.rb
@@ -1,49 +1,61 @@
-require "rubygems"
-require "sinatra"
-require "sinatra/reloader"
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
 
-get "/" do
-  erb "Вы на сайте где можно записаться на стрижку"
+get '/' do
+  erb 'Вы на сайте где можно записаться на стрижку'
 end
 
-get "/about" do
+get '/about' do
+
   erb :about
 end
 
-get "/visit" do
+get '/visit' do
   erb :visit
 end
 
-get "/login" do
+get '/login' do
   erb :login
 end
 
-post "/visit" do
+post '/visit' do
   @username = params[:username]
   @phone = params[:phone]
   @datetime = params[:datetime]
   @barber = params[:barber]
   @color = params[:color]
 
-  File.open("./public/users.txt", "a") do |file|
-    file.write "#{@username} - #{@phone} - #{@datetime} - #{@barber} - #{@color}"
-    file.write "\n"
+  hh = {
+    username: 'Введите имя',
+    phone: 'Введите телефон',
+    datetime: 'Введите дату и время'
+  }
+
+  # Обработка ошибок
+  @error = hh.select { |key, _| params[key] == '' }.values.join(', ')
+
+  return erb :visit if @error != ''
+
+  File.open('./public/users.txt', 'a') do |file|
+    file.write "#{@username} - #{@phone} - #{@datetime} - #{@barber} - #{@color}\n"
   end
+
   erb "#{@username} - #{@phone} - #{@datetime} - #{@barber} - #{@color}"
 end
 
-post "/" do
+post '/' do
   @login = params[:login]
   @pass = params[:pass]
 
-  if @login == "admin" && @pass == "secret"
-    @error = "Вы вошли в свою учетную запись" 
+  if @login == 'admin' && @pass == 'secret'
+    @error = 'Вы вошли в свою учетную запись'
     erb :visit
-  elsif @login == "admin" && @pass == "admin"
-    @access_denied = "Хорошая попытка!, но неправильный пароль или логин"
+  elsif @login == 'admin' && @pass == 'admin'
+    @access_denied = 'Хорошая попытка!, но неправильный пароль или логин'
     erb :login
   else
-    @access_denied = "Введен неправильный логин или пароль"
+    @access_denied = 'Введен неправильный логин или пароль'
     erb :login
   end
 end
