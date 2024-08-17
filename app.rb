49ae65838d39997a652dee1b7cbf5cb745ee0745
dn.artego <dn.artego@gmail.com>
2024-08-17 16:37:33 +0300

    create db

--- app.rb
+++ app.rb
@@ -1,26 +1,38 @@
-require 'rubygems'
-require 'sinatra'
-require 'sinatra/reloader'
-require 'dotenv/load'
+require "rubygems"
+require "sinatra"
+require "sinatra/reloader"
+require "dotenv/load"
+require "sqlite3"
 
-get '/' do
-  erb 'Вы на сайте где можно записаться на стрижку'
+configure do
+  @db = SQLite3::Database.new "_base.db"
+  @db.execute 'CREATE TABLE IF NOT EXISTS "users" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "usename" TEXT,
+    "phone"	TEXT,
+    "datestamp"	TEXT,
+    "barber"	TEXT,
+    "color"	TEXT
+  );'
 end
 
-get '/about' do
+get "/" do
+  erb "Вы на сайте где можно записаться на стрижку"
+end
 
+get "/about" do
   erb :about
 end
 
-get '/visit' do
+get "/visit" do
   erb :visit
 end
 
-get '/login' do
+get "/login" do
   erb :login
 end
 
-post '/visit' do
+post "/visit" do
   @username = params[:username]
   @phone = params[:phone]
   @datetime = params[:datetime]
@@ -28,35 +40,35 @@ post '/visit' do
   @color = params[:color]
 
   hh = {
-    username: 'Введите имя',
-    phone: 'Введите телефон',
-    datetime: 'Введите дату и время'
+    username: "Введите имя",
+    phone: "Введите телефон",
+    datetime: "Введите дату и время",
   }
 
   # Обработка ошибок
-  @error = hh.select { |key, _| params[key] == '' }.values.join(', ')
+  @error = hh.select { |key, _| params[key] == "" }.values.join(", ")
 
-  return erb :visit if @error != ''
+  return erb :visit if @error != ""
 
-  File.open('./public/users.txt', 'a') do |file|
+  File.open("./public/users.txt", "a") do |file|
     file.write "#{@username} - #{@phone} - #{@datetime} - #{@barber} - #{@color}\n"
   end
 
   erb "#{@username} - #{@phone} - #{@datetime} - #{@barber} - #{@color}"
 end
 
-post '/' do
+post "/" do
   @login = params[:login]
   @pass = params[:pass]
 
-  if @login == ENV['LOGIN'] && @pass == ENV['PASS']
-    @error = 'Вы вошли в свою учетную запись'
+  if @login == ENV["LOGIN"] && @pass == ENV["PASS"]
+    @error = "Вы вошли в свою учетную запись"
     erb :visit
-  elsif @login == 'admin' && @pass == 'admin'
-    @access_denied = 'Хорошая попытка!, но неправильный пароль или логин'
+  elsif @login == "admin" && @pass == "admin"
+    @access_denied = "Хорошая попытка!, но неправильный пароль или логин"
     erb :login
   else
-    @access_denied = 'Введен неправильный логин или пароль'
+    @access_denied = "Введен неправильный логин или пароль"
     erb :login
   end
 end
